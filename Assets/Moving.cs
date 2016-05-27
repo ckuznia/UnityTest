d6a1bf61ef04738310d99cc7c6f4a1e7c2b7a377
Clay Kuznia <clay-kuznia@hotmail.com>
2016-05-27 17:09:05 -0600

    Initial files

---
+++ Assets/Moving.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+
+	private float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// If RIGHT ARROW is pressed
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			// Getting current position
+			Vector2 pos = this.transform.position;
+			// Changing position
+			pos.x += speed;
+			// Updating position
+			this.transform.position = pos;
+		}
+		// If LEFT ARROW is pressed
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			// Getting current position
+			Vector2 pos = this.transform.position;
+			// Changing position
+			pos.x -= speed;
+			// Updating position
+			this.transform.position = pos;
+		}
+		// If UP ARROW is pressed
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			// Getting current position
+			Vector2 pos = this.transform.position;
+			// Changing position
+			pos.y += speed;
+			// Updating position
+			this.transform.position = pos;
+		}
+		// If DOWN ARROW is pressed
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			// Getting current position
+			Vector2 pos = this.transform.position;
+			// Changing position
+			pos.y -= speed;
+			// Updating position
+			this.transform.position = pos;
+		}
+	}
+}
